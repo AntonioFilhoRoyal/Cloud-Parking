9b541544a5939aa356873a50cc5fbd1f406e9777
Antonio Filho <antoniofilhocastro001@gmail.com>
2022-11-14 08:28:58 -0300

    Construindo...

--- src/main/java/one/dioinnovation/parking/controller/CloudParkingController.java
+++ src/main/java/one/dioinnovation/parking/controller/CloudParkingController.java
@@ -1,15 +1,13 @@
-package one.dioinnovation.controller;
+package one.dioinnovation.parking.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class CloudParkingController {
-	
+
 	@GetMapping
 	public String home() {
 		return "Bem-vindos";
